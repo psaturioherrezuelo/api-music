b790b4d04e3e3abd45e601670aa7736ff9728e68
Pablo <Pablo@DESKTOP-DNHR1A7.lan>
2021-05-22 15:31:08 +0200

    Ventana error de conexion con la bbdd

--- src/controllers/Principal.java
+++ src/controllers/Principal.java
@@ -1,13 +1,15 @@
 package controllers;
 
 import java.awt.EventQueue;
+import java.sql.SQLException;
 
-import models.Database;
-import views.Insertar;
 import views.Login;
+import views.ErrorConexion;
+import models.Database;
 
 public class Principal {
-
+	
+	
 	public static void main(String[] args) {
 
 		/* ********************************************************************************
@@ -19,20 +21,38 @@ public class Principal {
 		EventQueue.invokeLater(new Runnable() {
 			
 			public void run() {
-			
+				
+				 Database bd = new Database();
 				try {
+					//Verificamos de que tenemos conexion con la base de datos
+					bd.conexion();
+					
+					try {
+						//Lamamos a la ventana de Login
+						Login frame = new Login();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						
+						e.printStackTrace();
+					
+					}
+					bd.getConn().close();	        	
+		        } catch (SQLException | ClassNotFoundException e) {
+		        	//En caso de Error mostramos una ventana emergente de error y con el error en cuestion
+					try {
+																			//enviamos el mensaje a la vista emergente
+						ErrorConexion frameErrorConexion = new ErrorConexion(e.getMessage());
+						frameErrorConexion.setVisible(true);
+						
+					} catch (Exception ee) {
+						
+						ee.printStackTrace();
+					
+					}
+					
+		        	
+		        }
 				
-					Login frame = new Login();
-					frame.setVisible(true);
-					
-//					Insertar a = new Insertar();
-//					a.setVisible(true);
-					
-				} catch (Exception e) {
-					
-					e.printStackTrace();
-				
-				}
 			
 			}
 			
---
+++ src/views/ErrorConexion.java
@@ -0,0 +1,118 @@
+package views;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+import controllers.ControllerFiles;
+import controllers.ControllerModels;
+
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Toolkit;
+
+public class ErrorConexion extends JFrame implements WindowListener, ActionListener, MouseListener {
+
+	private JPanel contentPane;
+	
+//	private ControllerModels controlador = new ControllerModels();
+	private ControllerFiles cf = new ControllerFiles();
+
+	public ErrorConexion(String Error) {
+		setBackground(new Color(0, 0, 0));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(ErrorConexion.class.getResource(cf.getIconoBlack())));
+		setResizable(false);
+		setTitle("Login Api Music Error Base de datos");
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 800, 120);
+		contentPane = new JPanel();
+		
+		contentPane.setBackground(new Color(217, 37, 17));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblUser = new JLabel(Error);
+		lblUser.setForeground(new Color(255, 255, 255));
+		lblUser.setHorizontalAlignment(SwingConstants.CENTER);
+		lblUser.setBounds(-100, -10, 990, 100);
+		contentPane.add(lblUser);
+		
+		this.setLocationRelativeTo(null);
+		
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		
+	}
+	
+}
